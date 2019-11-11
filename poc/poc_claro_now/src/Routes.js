ae2cea3855d6e7d940caea9c8aa492c8ff512eda
Elen Almeida <elenalmeida@outlook.com>
2019-11-11 14:36:28 -0300

    Criação de componentes

---
+++ poc/poc_claro_now/src/Routes.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+const Routes = () => {}
+
+export default Routes;
---
+++ poc/poc_claro_now/src/pages/Home/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <main>
+      <p>Home funcionando</p>
+    </main>
+  );
+};
+
+export default Home;
