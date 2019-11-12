9597b880bf8fbba1c109f7ec64422ff6c58c95c5
Elen Almeida <elenalmeida@outlook.com>
2019-11-12 12:03:49 -0300

    Resolvendo iteração do componente carrossel

--- poc/poc_claro_now/src/components/Carrossel/carrossel.js
+++ poc/poc_claro_now/src/components/Carrossel/carrossel.js
@@ -1,8 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "./carrossel.css";
 
 const Carrossel = () => {
+  const [arr, setArr] = useState([
+    {
+      link: "canal 01", 
+      title: "First slide label",
+      text: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+    },
+    {
+      link: "canal 02",
+      title: "Second slide label",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      link: "canal 03",
+      title: "Third slide label",
+      text: "Praesent commodo cursus magna, vel scelerisque nisl consectetur."
+    },
+    {
+      link: "canal 04",
+      title: "Esse eu fix por conta",
+      text: "Para ter certeza que estava funcionando :);"
+    }
+  ]);
+  
+
   const [index, setIndex] = useState(0);
   const [direction, setDirection] = useState(null);
 
@@ -12,32 +36,20 @@ const Carrossel = () => {
   };
 
   return (
-    <main className="mainCarousel">
-      <p>Carrossel falta ajustar</p>
+    <main className="mainCarousel">  
+      
       <Carousel
         activeIndex={index}
         direction={direction}
         onSelect={handleSelect}
         className="carrousel"
       >
-        <Carousel.Item>
-          <h1>First slide label</h1>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Item>
-        <Carousel.Item>
-          <h1>Second slide label</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Item>
-        <Carousel.Item>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Item>
-        <Carousel.Item>
-          <h3>Esse eu fix por conta</h3>
-          <p>Para ter certeza que estava funcionando :);</p>
-        </Carousel.Item>
+        {arr.map((e, i) => (
+          <Carousel.Item key={e.link}>
+            <h1>{e.title}</h1>
+            <p>{e.text}</p>           
+          </Carousel.Item>
+        ))}        
       </Carousel>
     </main>
   );
