f35d7589deb950acf791bab1e6bb20aa1b97c267
Alejandro Mesias <amesias@l:wqocalhost.localdomain>
2019-11-12 10:48:14 -0300

    Elemento de Video

--- poc/poc_claro_now/src/Routes.js
+++ poc/poc_claro_now/src/Routes.js
@@ -1,14 +1,20 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Switch, Route, /* Redirect */} from 'react-router-dom';
+import YouTube from './assets/youtube'
 
 const Home = lazy(() => import('./pages/Home/home'));
+const VideoShowCase = lazy(() => import('./pages/videoshowcase'));
 
 const Routes = () =>(
   <BrowserRouter>
     <Suspense fallback={<div>Loading...</div>}>
       <Switch>
         <Route exact path='/' component={Home}></Route>
-        {/* <Route exact path='/Login' component={Login}></Route> */}
+        <Route exact path='/video' render={() => 
+          <VideoShowCase 
+            mainVideo={YouTube.Ano2015.PanFinalRevezamento4x200} 
+            thumbVideos={[YouTube.Ano2015.CampeonatoPiscinaCurta, YouTube.Ano2015.Rio2016Natacao]} />}>   
+        </Route>
         {/* <Route exact path='/Register' component={Register}></Route> */}
         {/* <Route exact path='/Salao' component={Salao}></Route>  */}
         {/* <Route exact path='/Cozinha' component={Cozinha}></Route>        */}
