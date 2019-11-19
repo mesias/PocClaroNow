54c8e3d75b30cb5733e585d931269c44813d41ce
Elen Almeida <elenalmeida@outlook.com>
2019-11-19 13:41:38 -0300

    Inserção imagens modalidades

--- poc/poc_claro_now/src/pages/Home/home.js
+++ poc/poc_claro_now/src/pages/Home/home.js
@@ -6,22 +6,27 @@ import MedalsChart from "../../components/MedalsChart";
 import Modalidades from "../../components/esportes/sports";
 import MedalhaBrasil from "../../assets/img/home-medalha-brasil.png";
 import MedalhaUsa from "../../assets/img/home-medalha-eua.png";
-import MedalhaJp from '../../assets/img/home-medalha-japao.png';
-import MedalhaRs from '../../assets/img/home-medalha-russia.png';
-import MedalhaPt from '../../assets/img/home-medalha-portugal.png';
-import MedalhaAl from '../../assets/img/home-medalha-alemanha.png';
-import MedalhaFr from '../../assets/img/home-medalha-franca.png';
- 
+import MedalhaJp from "../../assets/img/home-medalha-japao.png";
+import MedalhaRs from "../../assets/img/home-medalha-russia.png";
+import MedalhaPt from "../../assets/img/home-medalha-portugal.png";
+import MedalhaAl from "../../assets/img/home-medalha-alemanha.png";
+import MedalhaFr from "../../assets/img/home-medalha-franca.png";
+import BasqueteM from "../../assets/img/home-modalidade-basquete.png"
+import BoxeM from "../../assets/img/home-modalidade-boxe.png";
+import FutebolM from "../../assets/img/home-modalidade-futebol.png";
+import MergulhoM from "../../assets/img/home-modalidade-mergulho.png";
+import VoleibalM from "../../assets/img/home-modalidade-volei.png";
+
 const Home = () => {
   return (
     <main className="container-main">
       <section className="container-nav">
         <NavBarHome></NavBarHome>
-      </section>
-      <section>
-        <h5>AO VIVO</h5>
-      </section>
+      </section>      
       <section className="sectionCarrossel">
+      <section className='contTitle'>
+        <h5 className='title'>AO VIVO</h5>
+      </section>
         <Menu></Menu>
       </section>
       <section className="containerMedalhas">
@@ -45,7 +50,7 @@ const Home = () => {
           <button className="buttonHome">
             <img src={MedalhaAl} className="imgbtn"></img>
           </button>
-          <button className="buttonHome">           
+          <button className="buttonHome">
             <img src={MedalhaFr} className="imgbtn"></img>
           </button>
         </section>
@@ -54,11 +59,21 @@ const Home = () => {
         <section className=" containerMedalhas containerModalidades">
           <h5>MODALIDADES EM DESTAQUE</h5>
           <section className="btnContMod">
-            <button className="buttonModalidades"></button>
-            <button className="buttonModalidades"></button>
-            <button className="buttonModalidades"></button>
-            <button className="buttonModalidades"></button>
-            <button className="buttonModalidades"></button>
+            <button className="buttonModalidades">
+              <img src={BasqueteM} className="imgbtnMod"></img>
+            </button>
+            <button className="buttonModalidades">
+              <img src={BoxeM} className="imgbtnMod"></img>
+            </button>
+            <button className="buttonModalidades">
+              <img src={FutebolM} className="imgbtnMod"></img>
+            </button>
+            <button className="buttonModalidades">              
+              <img src={MergulhoM} className="imgbtnMod"></img>
+            </button>
+            <button className="buttonModalidades">             
+              <img src={VoleibalM} className="imgbtnMod"></img>
+            </button>
           </section>
         </section>
       </section>
